c24586f9b7de300713e7cbefbde5c8e315ac1017
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2023-08-25 18:27:04 +0200

    🤾Home.py

--- 🤾Home.py
+++ 🤾Home.py
@@ -7,7 +7,7 @@ from PIL import Image
 st.set_page_config(page_title="HDL", page_icon="favicon-32x32.png", layout="wide")
 
 image = Image.open('HDL-blanc.png')
-st.image(image, use_column_width=True) 
+st.image(image) 
     
 
 
