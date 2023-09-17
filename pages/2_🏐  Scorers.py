45d7de81580ab49d5ba60f31aadf09c88778f561
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2023-09-17 21:06:52 +0200

    🏐  Scorers.py

--- pages/2_🏐  Scorers.py
+++ pages/2_🏐  Scorers.py
@@ -7,6 +7,7 @@ import altair as alt
 from vega_datasets import data
 import matplotlib.colors as mcolors
 
+
 # Configura el título de la página i favicon
 st.set_page_config(page_title="Scorers", page_icon="ball.png", layout="wide")
 st.title('🏐Scorers')
@@ -14,23 +15,34 @@ st.header('🎯Goleadores Asobal')
 st.subheader('📌Consulta todos los goleadores según **equipo**:')
 
 df = pd.read_excel("DatasetJugadoresAsobal.xlsx")
+df1 = pd.read_excel("DataJugadoresAsobal2324.xlsx")
 
-## Conseguir lista de equipos
-Equipos = df['Equipo'].unique()
 
-## Create the select box
-selected_team = st.selectbox('Escoge equipo:', Equipos)
+# Obtener una lista de temporadas únicas de ambos DataFrames
+temporadas = pd.concat([df1['Temporada'], df['Temporada']]).unique()
 
-## Filter the data
-filtered_data = df[df['Equipo'] == selected_team]
+# Crear el select box para la temporada
+selected_temporada1 = st.selectbox('Escoge una temporada:', temporadas, key="selectbox1")
 
-## Generate unique colors for each team (combining multiple color scales)
-team_colors1 = dict(zip(Equipos[:8], mcolors.TABLEAU_COLORS.values()))
-team_colors2 = dict(zip(Equipos[8:16], mcolors.XKCD_COLORS.values()))
+# Filtrar los datos según la temporada seleccionada desde ambos DataFrames
+filtered_data1 = df[df['Temporada'] == selected_temporada1].append(df1[df1['Temporada'] == selected_temporada1])
+
+# Obtener una lista de equipos únicos para la temporada seleccionada
+equipos_temporada = filtered_data1['Equipo'].unique()
+
+# Crear el select box para el equipo
+selected_equipo = st.selectbox('Escoge un equipo:', equipos_temporada)
+
+# Filtrar los datos nuevamente para mostrar solo el equipo seleccionado
+filtered_data11 = filtered_data1[filtered_data1['Equipo'] == selected_equipo]
+
+# Generar colores únicos para cada equipo (combina múltiples escalas de colores)
+team_colors1 = dict(zip(equipos_temporada[:8], mcolors.TABLEAU_COLORS.values()))
+team_colors2 = dict(zip(equipos_temporada[8:16], mcolors.XKCD_COLORS.values()))
 team_colors = {**team_colors1, **team_colors2}
 
-## Graph
-graph = alt.Chart(filtered_data).encode(
+# Crear el gráfico
+graph = alt.Chart(filtered_data11).encode(
     x='ToG',
     y=alt.Y("Jugador").sort('-x'),
     text='ToG',
@@ -38,9 +50,11 @@ graph = alt.Chart(filtered_data).encode(
     color=alt.Color("Equipo", scale=alt.Scale(domain=list(team_colors.keys()), range=list(team_colors.values())))
 )
 plotfinal = graph.mark_bar() + graph.mark_text(align='left', dx=2)
+
+# Mostrar el gráfico en Streamlit
 st.altair_chart(plotfinal, use_container_width=True)
 
-st.caption("🔎Data: Asobal via Handball AI")
+st.caption("🔎Fuente: Asobal")
 expander = st.expander(" ➕ **LEGEND**")
 expander.write("**ToG** = Total Goles Marcados")
 expander.write("**ToS** = Total Lanzamientos Intentados")
@@ -49,17 +63,32 @@ expander.write("**To%** = Porcentaje de acierto en el lanzamiento")
 st.divider()
 
 st.subheader('📌Consulta todos los goleadores según **posición**:')
-## Conseguir lista de posiciones
-Pos = df['Posicion'].unique()
 
-## Create the select box (filter)
-selected_pos = st.selectbox('Selecciona posición:', Pos)
+# Crear el filtro de temporada para el segundo gráfico en el cuerpo principal
+selected_temporada2 = st.selectbox('Escoge una temporada:', temporadas, key="selectbox2")
 
-## Filter the data
-filtered_data = df[df['Posicion'] == selected_pos]
+# Filtrar los datos según la temporada seleccionada desde ambos DataFrames
+filtered_data2 = df[df['Temporada'] == selected_temporada2].append(df1[df1['Temporada'] == selected_temporada2])
+
+# Obtener una lista de equipos únicos para la temporada seleccionada
+equipos_temporada2 = filtered_data2['Equipo'].unique()
+
+# Obtener una lista de posiciones únicos para la temporada seleccionada
+pos_temporada = filtered_data2['Posicion'].unique()
+
+# Crear el select box para la posicion
+selected_pos = st.selectbox('Escoge una posicion:', pos_temporada)
+
+# Filtrar los datos nuevamente para mostrar solo  seleccionado
+filtered_data22 = filtered_data2[filtered_data2['Posicion'] == selected_pos]
+
+# Generar colores únicos para cada equipo (combina múltiples escalas de colores)
+team_colors1 = dict(zip(equipos_temporada2[:8], mcolors.TABLEAU_COLORS.values()))
+team_colors2 = dict(zip(equipos_temporada2[8:16], mcolors.XKCD_COLORS.values()))
+team_colors = {**team_colors1, **team_colors2}
 
 ## Graph
-graph = alt.Chart(filtered_data).encode(
+graph = alt.Chart(filtered_data22).encode(
     x='ToG',
     y=alt.Y("Jugador").sort('-x'),
     text='ToG',
@@ -69,7 +98,7 @@ graph = alt.Chart(filtered_data).encode(
 plotfinalpos = graph.mark_bar() + graph.mark_text(align='left', dx=2)
 st.altair_chart(plotfinalpos, use_container_width=True)
 
-st.caption("🔎Data: Asobal via Handball AI")
+st.caption("🔎Fuente: Asobal")
 expander = st.expander(" ➕ **LEGEND**")
 expander.write("**ToG** = Total Goles Marcados")
 expander.write("**ToS** = Total Lanzamientos Intentados")
