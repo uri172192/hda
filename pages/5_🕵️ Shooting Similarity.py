449406c83381f35437225772b45a3a87f3da3a17
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2023-09-18 19:11:56 +0200

    🕵️ Shooting Similarity.py

--- pages/5_🕵️ Shooting Similarity.py
+++ pages/5_🕵️ Shooting Similarity.py
@@ -1,73 +1,4 @@
-import streamlit as st 
-# Configura el título de la página i favicon
-st.set_page_config(page_title="Shooting Similarity", page_icon="detective.png", layout="wide")
-st.title('🕵️Similitud Jugadores')
-st.subheader('📌Descubre los jugadores más similares entre si respecto a su eficacia en el lanzamiento en la Liga Asobal.')
-
-import pandas as pd
-import numpy as np
-import plotly.express as px
-import plotly.graph_objects as go
-import altair as alt
-from vega_datasets import data
-#-------------------------------------
-df = pd.read_excel("DatasetJugadoresAsobal.xlsx")
-#-------------------------------------
-
-
-# Librerías
-
-import pandas as pd
-pd.set_option('display.max_columns', None)
-pd.set_option('display.max_rows', None)
-import numpy as np
-
-from matplotlib import pyplot as plt
-
-import warnings
-warnings.filterwarnings(action="ignore")
-
-from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
-
-#Cambiar orden posicion metricas object:
-df.insert(1,'Jugador', df.pop("Jugador"))
-df.insert(2,'Posicion', df.pop("Posicion"))
-
-#Eliminar columnas:
-df.drop(columns='Nº', inplace=True)
-df.drop(columns='Equipo', inplace=True)
-df.drop(columns='Posicion', inplace=True)
-df.drop(columns='Temporada', inplace=True)
-
-X, y = df.iloc[:, 1:len(df.columns)].values, df.iloc[:, 0].values
-X_std = StandardScaler().fit_transform(X)
-
-pca = PCA(n_components = len(df.columns)-1)
-pca.fit(X_std)
-X_pca = pca.transform(X_std)
-
-print("Shape x_PCA: ", X_pca.shape)
-expl = pca.explained_variance_ratio_
-
-
-#--------------
-
-N_COMP = 8
-columns = []
-
-for col in range(1, N_COMP+1, 1):
-    columns.append("PCA" + str(col))
-
-df_pca_resultado = pd.DataFrame(data=X_pca[:,0:N_COMP], columns=columns, index = y)
-
-df_pca_resultado.head()
-
-corr_matrix = df_pca_resultado.T.corr(method='pearson')
-
-corr_matrix.head()
-
-## Conseguir llista de equipsimport streamlit as st
+import streamlit as st
 import pandas as pd
 import numpy as np
 import plotly.express as px
@@ -178,16 +109,6 @@ if selected_player:
     # Display the resulting DataFrame
     st.write(df_correlatedPlayers)
 
-
-
-#df_correlatedPlayers = GetSimilarPlayers(Jugador, NumPlayers, corr_matrix)
-
-#df_correlatedPlayers
-
-
-
-
-df_correlatedPlayers = GetSimilarPlayers(selected_player, num_players, corr_matrix)
 st.write(df_correlatedPlayers)
 st.caption("🔎Data: Asobal via Handball AI")
 st.caption('🔎Datos correspondientes a la temproada 22-23 de la liga Asobal.')
