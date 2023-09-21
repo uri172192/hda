277b976a011744924e034aa3c0b2e009474159ec
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2023-09-21 21:38:13 +0200

    🗂️ Data Consulting.py

--- pages/7_🗂️ Data Consulting.py
+++ pages/7_🗂️ Data Consulting.py
@@ -6,23 +6,35 @@ st.set_page_config(page_title="Data Consulting", page_icon="folder.png", layout=
 st.title("🗂️Data Consulting")
 
 st.subheader('📌Players Data')
-st.write('Consulta datos jugadores Asobal 22/23:')
+st.write('Consulta datos jugadores Asobal:')
 ## Df Load
 df = pd.read_excel("DatasetJugadoresAsobal.xlsx")
+df1 = pd.read_excel("DataJugadoresAsobal2324.xlsx")
 
-## Get the list of teams
-Equipos = df['Equipo'].unique()
- 
-## Create the select box
-selected_team = st.selectbox('Selecciona equipo:', Equipos)
- 
-## Filter the data
-filtered_data = df[df['Equipo'] == selected_team]
+
+# Obtener una lista de temporadas únicas de ambos DataFrames
+temporadas = pd.concat([df1['Temporada'], df['Temporada']]).unique()
+
+# Crear el select box para la temporada
+selected_temporada1 = st.selectbox('Escoge una temporada:', temporadas, key="selectbox1")
+
+# Filtrar los datos según la temporada seleccionada desde ambos DataFrames
+filtered_data1 = df[df['Temporada'] == selected_temporada1].append(df1[df1['Temporada'] == selected_temporada1])
+
+# Obtener una lista de equipos únicos para la temporada seleccionada
+equipos_temporada = filtered_data1['Equipo'].unique()
+
+# Crear el select box para el equipo
+selected_equipo = st.selectbox('Escoge un equipo:', equipos_temporada)
+
+# Filtrar los datos nuevamente para mostrar solo el equipo seleccionado
+filtered_data11 = filtered_data1[filtered_data1['Equipo'] == selected_equipo]
+
  
 ## Display the filtered data
-st.write(filtered_data)
+st.write(filtered_data11)
 
-st.caption("🔎Data: Asobal via Handball AI")
+st.caption("🔎Fuente: Asobal")
 
 #Legend
 expander = st.expander("➕ **LEGEND**")
@@ -37,15 +49,29 @@ expander.write("**L6 (Lanzamientos de 6 metros), L9 (Lanzamientos de 9 metros), 
 st.divider()
 
 st.subheader('📌Teams Data')
-st.write('Consulta datos equipos Asobal 22/23:')
+st.write('Consulta datos equipos Asobal:')
 dfteams = pd.read_excel("DatasetEquiposAsobal.xlsx")
+dfteams1 = pd.read_excel("DatasetEquiposAsobal2324.xlsx")
+
+# Obtener una lista de temporadas únicas de ambos DataFrames
+temporadas = pd.concat([dfteams1['Temporada'], dfteams['Temporada']]).unique()
+
+# Crear el select box para la temporada
+selected_temporada1 = st.selectbox('Escoge una temporada:', temporadas, key="selectbox2")
+
+# Filtrar los datos según la temporada seleccionada desde ambos DataFrames
+filtered_data1 = dfteams[dfteams['Temporada'] == selected_temporada1].append(dfteams1[dfteams1['Temporada'] == selected_temporada1])
+ 
+
+
+
 def round_table_values(df):
     # Aplica redondeo a 2 decimales para todas las celdas del DataFrame
-    rounded_df = df.round(2)
+    rounded_df = filtered_data1.round(2)
     return rounded_df
-df_rounded = round_table_values(dfteams)
+df_rounded = round_table_values(filtered_data1)
 st.write(df_rounded)
-st.caption("🔎Data: Asobal via Handball AI")
+st.caption("🔎Fuente: Asobal")
 
 expander = st.expander(" ➕ **LEGEND**")
 expander.write("**Equipo** = Nombre del equipo")
