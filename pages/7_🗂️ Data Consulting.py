21ca3bbba6773b0f78d1a90164e30efbb6b31eca
Oriol Prat Barceló <138971548+uri172192@users.noreply.github.com>
2023-09-21 22:14:13 +0200

    🗂️ Data Consulting.py

--- pages/7_🗂️ Data Consulting.py
+++ pages/7_🗂️ Data Consulting.py
@@ -62,9 +62,9 @@ selected_temporada1 = st.selectbox('Escoge una temporada:', temporadas, key="sel
 filtered_data1 = dfteams[dfteams['Temporada'] == selected_temporada1], (dfteams1[dfteams1['Temporada'] == selected_temporada1])
  
 
-def round_table_values(filtered_data1):
+def round_table_values(df):
     # Aplica redondeo a 2 decimales para todas las celdas del DataFrame
-    rounded_df = filtered_data1.round(2)
+    rounded_df = dfteams.round(2)
     return rounded_df
 df_rounded = round_table_values(filtered_data1)
 st.write(df_rounded)
